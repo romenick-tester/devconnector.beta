860674a060d39d85f7e24c935dc35de591fdd2d7
Romenick Tester <romenick.tester@gmail.com>
2021-02-17 16:09:37 +0000

    express-validator api request validations

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,4 +1,11 @@
+const { validationResult } = require("express-validator");
+
 const registerUser = (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array().map((error) => error.msg) });
+    }
+
     console.log(req.body);
     res.send("users route");
 }
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require("express-validator");
 const {
-    registerUser } = require("../../controllers/usersControllers");
+    registerUser 
+} = require("../../controllers/usersControllers");
+
+const registerValidation = [
+    check("name", "Name is required.").not().isEmpty(),
+    check("email", "Enter a valid email.").isEmail(),
+    check("password", "Enter a password with 6 or more characters").isLength({ min: 6 })
+]
 
 //route:        POST api/users
 //desc:         register user
 //access:       public  
-router.post("/", registerUser);
+router.post("/", [...registerValidation], registerUser);
 
 module.exports = router;
