c7bb4b1b61c468d54a7e194d16bb1f9a3a8eed49
Romenick Tester <romenick.tester@gmail.com>
2021-02-17 16:51:30 +0000

    register user

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,13 +1,48 @@
 const { validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
-const registerUser = (req,res) => {
+const registerUser = async(req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array().map((error) => error.msg) });
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log(req.body);
-    res.send("users route");
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(user) {
+            return res.status(400).json({ errors: [ { msg: "User already exist!" } ] });
+        }
+
+        const avatar = gravatar.url(email, {
+            s: "200",
+            r: "pg",
+            d: "mm"
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+
+        const salt = await bcrypt.genSalt(10);
+        
+        user.password = await bcrypt.hash(password, salt);
+    
+        await user.save();
+    
+        // return jsonwebtoken
+        res.send("user registered!");
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
 }
 
 module.exports = { registerUser };
