29fdea007a640fd200a421a089467362488c746b
Romenick Tester <romenick.tester@gmail.com>
2021-02-24 01:33:27 +0000

    registerdisplay component setup

--- client/src/displays/RegisterDisplay.js
+++ client/src/displays/RegisterDisplay.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function RegisterDisplay() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    function changeHandler(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+        if(password !== password2) {
+            console.log("password does not match!");
+            return;
+        }
+        console.log(formData);
+    }
+
     return (
-        <div>
-            <h5>register component</h5>
-        </div>
+        <>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={submitHandler}>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        name="name"
+                        value={name}
+                        onChange={(e) => changeHandler(e)} 
+                        required />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="email" 
+                        placeholder="Email Address" 
+                        name="email"
+                        value={email}
+                        onChange={(e) => changeHandler(e)} />
+                    <small className="form-text">
+                    This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={(e) => changeHandler(e)} />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    minLength="6"
+                    value={password2}
+                    onChange={(e) => changeHandler(e)} />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </>
     )
 }
 
