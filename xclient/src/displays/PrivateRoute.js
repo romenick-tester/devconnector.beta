95f2584fb4c80b33e9f3f69d37e9e9c89b8eda26
Romenick Tester <romenick.tester@gmail.com>
2021-03-02 11:32:28 +0000

    privateroute component

---
+++ xclient/src/displays/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function PrivateRoute({ component: Component, ...rest }) {
+    const { auth_loading, isAuthenticated } = useSelector(state => state.auth);
+
+    return <Route {...rest} render={
+        (props) => !auth_loading && isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />}
+    />
+}
+
+export default PrivateRoute
